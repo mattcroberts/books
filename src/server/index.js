bda10be470c6eec54f34f90700de6f04ed0f2938
mattr <matt.roberts@thetrainline.com>
2018-06-11 17:56:38 +0100

    templating

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import { renderToString } from "react-dom/server";
+import { join } from "path";
 
 const app = express();
-
+app.set("views", join(__dirname, "./views"));
 app.get("/books", (req, res, next) => {
     res.json({});
 });
-app.get((req, res, next) => {
+app.get("*", (req, res, next) => {
     // render react
+    res.render("index.ejs", {
+        app: "bam"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Listening on 3000");
 });
