1313974cd2bf2fb04d8fe554c753a23c0bc8b823
mattr <matt.roberts@thetrainline.com>
2018-06-12 15:44:03 +0100

    unit tests

--- src/shared/reducers/books/book.test.js
+++ src/shared/reducers/books/book.test.js
@@ -1,7 +1,8 @@
 import { expect } from "chai";
 
 import reducer from "./index";
-import { fetchStarted, fetchFinished } from "../../actions";
+import { fetchStarted, fetchFinished, fetchError } from "../../actions";
+import data from "./fixtures/bookResponse.json";
 
 describe("Book reducer", () => {
     let state;
@@ -30,7 +31,27 @@ describe("Book reducer", () => {
 
     it("should set loading flag=true when fetch ends", () => {
         expect(
-            reducer(undefined, fetchFinished({ results: [] }))
+            reducer({ loaded: false }, fetchFinished({ results: [] }))
         ).to.have.property("loaded", true);
+    });
+
+    it("should set loading flag=true when fetch errors", () => {
+        expect(
+            reducer({ loaded: false }, fetchError(new Error()))
+        ).to.have.property("loaded", true);
+    });
+
+    it("should store book data in state", () => {
+        const result = reducer(undefined, fetchFinished(data));
+
+        expect(result).to.have.nested.property("books.length", 2);
+        expect(result).to.have.deep.nested.property("books[0]", {
+            author: "Diana Gabaldon",
+            description:
+                "The author of the Outlander novels gives tips on writing sex scenes, drawing on examples from the books.",
+            isbn10: "0399178570",
+            isbn13: "9780399178573",
+            title: '"I GIVE YOU MY BODY ..."'
+        });
     });
 });
